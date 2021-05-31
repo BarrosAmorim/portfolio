1b2f1c3979c394eb259fa3c10270bf9cb6d21707
BarrosAmorim <barrosamorimd@gmail.com>
2021-05-31 11:40:29 -0300

    Projects Page final

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -19,26 +19,26 @@ const ProjectCard: FunctionComponent<{ project: ProjectProps }> = ({
             <img src={image_path} alt={name} className="cursor-pointer" onClick={() => setShowDetail(true)} />
             <p className="my-2 text-center">{name}</p>
             {showDetail && (
-                <div className="absolute top-0 left-0 z-10 grid w-full h-auto text-black bg-gray-100 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100">
+                <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-1 text-black bg-gray-100 md:grid-cols-2 gap-x-6 dark:text-white dark:bg-dark-100">
                     <div>
                         <img src={image_path} alt={name} />
-                        <div className="flex justify-center my-4 space-x-3">
-                            <a href={github_url}  className="items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200">
+                        <div className="flex my-4 space-x-3">
+                            <a href={github_url}  className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200">
                                 <AiFillGithub />
                                 <span>GitHub</span>
                             </a>
-                            <a href={deployed_url} className="items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200">
+                            <a href={deployed_url} className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200">
                                 <AiFillProject />
                                 <span>Projeto</span>
                             </a>
                         </div>
                     </div>
-                    <div>
+                    <div className="my-5">
                         <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
                         <h3 className="mb-3 font-medium">{description}</h3>
                         <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
                             {
-                                key_techs.map((tech) => <span key={tech} className="px-2 py-1 my-1 bg-gray-200">{tech}</span>)
+                                key_techs.map((tech) => <span key={tech} className="px-1 py-1 my-1 bg-gray-200 rounded dark:bg-dark-200">{tech}</span>)
                             }
                         </div>
                     </div>
